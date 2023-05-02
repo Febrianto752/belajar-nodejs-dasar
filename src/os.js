7cf0f3f1af8e4bbb7c9ad8b890a60341d95c8ce4
Febrianto <febrianto.bekasi@gmail.com>
2023-05-02 11:15:00 +0700

    learn os package

---
+++ src/os.js
@@ -0,0 +1,8 @@
+import os from "os";
+
+console.log(os.platform()); // output : win32
+console.log(os.arch()); // output : x64
+console.log(os.uptime()); // output : 530569
+console.log(os.totalmem()); // output : 17042952192 byte = 16gb
+console.log(os.freemem()); // output : 9718980608 byte
+console.table(os.networkInterfaces());
